24e7a29aaab1665c262a41ea1075c445cb655fb3
hackemate_ninja <nitrowitty@gmail.com>
2020-03-11 12:58:57 -0600

    print

--- src/app/dashboard/print/print.component.ts
+++ src/app/dashboard/print/print.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {GetStoresService} from '../../services/get-stores.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-print',
@@ -7,13 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrintComponent implements OnInit {
 
-  constructor() { }
+  store;
+  orderId;
+  orderInfo: any;
+  orderNumber: string;
+  submittedTime: string;
+  orderState: string;
+  shippingGroup: any;
+  commerceItems: any;
+
+  constructor(private order: GetStoresService, private route: ActivatedRoute,) { }
 
   ngOnInit(): void {
+    this.getOrder(this.getOrderId(), this.getStoreId());
   }
 
   goBack(){
     window.history.back();
+  }
+
+  getStoreId(): number {
+    this.route.paramMap.subscribe(params => {
+      this.store = params.get('store');
+    });
+    return this.store;
+  }
+
+  getOrderId(): number {
+    this.route.paramMap.subscribe(params => {
+      this.orderId = params.get('id');
+    });
+    return this.orderId;
+  }
+  getOrder(id, store) {
+    this.order.getOrderById(id, store)
+      .subscribe(res => {
+        this.orderInfo = res.result;
+        this.orderNumber = this.orderInfo.orderNumber.toString();
+        this.orderState = this.orderInfo.stateAsString.toString();
+        this.submittedTime = this.orderInfo.submittedTime.toString();
+        this.shippingGroup =  this.orderInfo.shippingGroups;
+        this.commerceItems = this.orderInfo.commerceItems;
+        return this.orderInfo;
+      }, error => {
+        return this.orderInfo
+      });
   }
 
 }
