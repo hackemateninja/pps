1311d8323daf5428224e130b017e90f19b5d7191
hackemate_ninja <nitrowitty@gmail.com>
2020-03-11 19:04:19 -0600

    latest changes

--- src/app/dashboard/order/order.component.ts
+++ src/app/dashboard/order/order.component.ts
@@ -18,6 +18,7 @@ export class OrderComponent implements OnInit {
   commerceItems: any;
   numberOfBoxes: string;
   boxes = [];
+  itemStatus: string;
 
   constructor(private order: GetStoresService, private route: ActivatedRoute,) { }
 
@@ -34,18 +35,19 @@ export class OrderComponent implements OnInit {
     if (this.boxes.length >= 1){
       this.boxes = [];
     }
-    for (let i = 0; i <= parseInt(this.numberOfBoxes); i++){
+    for (let i = 1; i <= parseInt(this.numberOfBoxes); i++){
       this.boxes.push(i);
     }
     return this.numberOfBoxes;
   }
 
-  /*setNumberOfBoxes(event: any): any{
-    for (let i = 0; i <= parseInt(this.numberOfBoxes); i++){
-      this.boxes.push(i);
+  getItemStatus(event: any): void{
+    this.itemStatus = event.target.value.toString();
+    console.log(typeof this.itemStatus);
+    if (this.itemStatus === "1"){
+
     }
-    return this.boxes;
-  }*/
+  }
 
   getStoreId(): number {
     this.route.paramMap.subscribe(params => {
