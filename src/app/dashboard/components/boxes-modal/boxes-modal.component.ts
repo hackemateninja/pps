ddbecf437e7b38d0ee2df79c97d0569e9e172ac1
hackemate_ninja <nitrowitty@gmail.com>
2020-05-12 07:38:50 -0600

    some changes

--- src/app/dashboard/components/boxes-modal/boxes-modal.component.ts
+++ src/app/dashboard/components/boxes-modal/boxes-modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-boxes-modal',
@@ -12,14 +12,19 @@ export class BoxesModalComponent implements OnInit {
   alertType: string;
   alertMessage: string;
   showAlert = false;
+  @Output() closeModalEmmit = new EventEmitter<boolean>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  setBoxes(){
+  closeModal(option: boolean){
+    this.closeModalEmmit.emit(option);
+  }
 
+  setBoxes(){
+    this.closeModal(this.showModal);
   }
 
 }
